3f43acfc277411c7cba366b9932dacac665beea7
taisan <131413638+taisan11@users.noreply.github.com>
2023-12-03 18:55:52 +0900

    fmt and etc..

--- cmd.ts
+++ cmd.ts
@@ -7,7 +7,7 @@ import { transformCode } from "./mod.ts";
 const kuro = 20;
 
 // const oldCode = `imp(kuro)\na=1+1\nb=2\nkuro+b`;
-const newCode = transformCode(data, { kuro });  // 変数のマップを渡す
+const newCode = transformCode(data, { kuro }); // 変数のマップを渡す
 console.log(newCode);
 const Code = newCode + "\nadd();";
 // 実行
--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ import $ from "https://deno.land/x/dax@0.35.0/mod.ts";
 
 // MOD
 const kuro = 20;
-const miritime = Date.now()
+const miritime = Date.now();
 
 const oldCode = `
 a = 1279828
 a+60
 `;
-const newCode = transformCode(oldCode, { kuro,miritime },"add");  // 変数のマップを渡す
+const newCode = transformCode(oldCode, { kuro, miritime }, "add"); // 変数のマップを渡す
 console.log(newCode);
 const Code = newCode + "\nadd();";
 // 実行
