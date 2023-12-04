f4127f54e2876eb701ec863dc0ebcc4bf52fd9f4
taisan <131413638+taisan11@users.noreply.github.com>
2023-12-04 16:50:02 +0900

    way

--- module/errorhand.ts
+++ module/errorhand.ts
@@ -8,14 +8,31 @@
  * console.log(result);
  * @returns シンプルなJScode.
  */
-export function errorhand(code: string, vars: Record<string, any>,name: string): string {
-    // コードを行に分割
-    const lines = code.split('\n');
-  
-    // 新しいコードを生成するための配列
-    const newCode = [];
-  
-    // 各行を解析
-    for (const line of lines) {
-        
+function isFullWidthNumber(str: string) {
+  return /^[０-９]+$/.test(str);
+}
+export function errorhand(
+  code: string,
+  vars: Record<string, any>,
+  name: string,
+): string {
+  // コードを行に分割
+  const lines = code.split("\n");
+
+  // 新しいコードを生成するための配列
+  const newCode = [];
+
+  // 各行を解析
+  for (const line of lines) {
+    // 変数定義の行を解析
+    const varDefMatch = line.match(/^(\w+)\s*=\s*(.+)$/);
+    if (varDefMatch) {
+      if (isFullWidthNumber(varDefMatch[1])) {
+        return `Error: Variable "${varDefMatch[1]}" is FullWidthNumber`;
+      }
+      continue;
+    }else{
+      return `Error: Variable "${varDefMatch[1]}" not found`;
+    }
   }
+}
