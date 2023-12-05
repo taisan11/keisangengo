824032570366abe5e513950d868eed16e2daa396
taisan <131413638+taisan11@users.noreply.github.com>
2023-12-05 17:59:27 +0900

    o

---
+++ test/test.ts
@@ -0,0 +1,13 @@
+// import { assertEquals } from "https://deno.land/std@0.204.0/testing/asserts.ts";
+
+// Deno.test("簡単な足し算", () => {
+//   // const actual = sum(1, 2);
+//   const expected = 3;
+//   // assertEquals(actual, expected);
+// });
+
+// Deno.test("sum() returns 0 when no arguments given", () => {
+//   // const actual = sum();
+//   // const expected = 0;
+//   // assertEquals( expected);
+// });
